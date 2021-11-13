1c4dc1309ad2a08638bcd7a21bb67e7b176e37d8
Nahid9n <91003844+Nahid9n@users.noreply.github.com>
2021-11-13 15:11:22 +0600

    Lab 7 final

---
+++ Lab 7/css.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Lab 7</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>	<br>
+		<div class="list">
+			<ul>
+				
+				<li><a href="home.php">Home</li></a>
+				<li><a href="article.php">Articles</li></a>
+				<li><a href="php.php">PHP Tips & Tricks</li></a></a></a>
+				<li><a href="css.php">CSS </li></a></a>
+				<li><a href="login.php">Login</li></a>
+			</ul>
+		</div>
+		
+		<div class="article">
+			<h2>What is CSS ?</h2>
+			<p>	CSS stands for Cascading Style Sheets</p>
+			<p>	CSS describes how HTML elements are to be displayed on screen, paper, or in other media</p>
+			<p>	CSS saves a lot of work. It can control the layout of multiple web pages all at once</p>
+			<p>External stylesheets are stored in CSS files
+	</p>
+			
+
+			<h2>Why Use CSS?</h2>
+			<p>CSS is used to define styles for your web pages, including the design, layout and variations in display for different devices and screen sizes.</p>
+
+			<h2>CSS Solved a Big Problem</h2>
+			<p>HTML was NEVER intended to contain tags for formatting a web page!
+
+			HTML was created to describe the content of a web page, like:
+
+This is a heading
+
+			<p>This is a paragraph.</p>
+
+			<p>When tags like, and color attributes were added to the HTML 3.2 specification, it started a nightmare for web developers. Development of large websites, where fonts and color information were added to every single page, became a long and expensive process.
+
+			To solve this problem, the World Wide Web Consortium (W3C) created CSS.
+
+			CSS removed the style formatting from the HTML page!</p>
+
+			<h2></h2>
+			<p></p>
+
+			<h2></h2>
+			<p></p>
+		</div>
+
+		<!-- Footer section Starts -->
+	<div class="footer">
+		<div class="wrapper">
+			<p class="text-center">2020 All rights Developed By -<a href="https://www.facebook.com/tanvirhassan.nahid/">NAHID</a></p>
+		</div>
+		
+	</div>
+	<!-- Footer section Ends -->
+
+
+
+</body>
+</html>
---
+++ Lab 7/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Lab 7</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>	<br>
+		<h1>Welcome to PHP & CSS</h1>
+		<div class="list">
+			<ul>
+				<li><a href="home.php">Home</li></a>
+				<li><a href="article.php">Articles</li></a>
+				<li><a href="php.php">PHP Tips & Tricks</li></a></a></a>
+				<li><a href="css.php">CSS </li></a></a>
+				<li><a href="login.php">Login</li></a>
+			</ul>
+		</div>
+		
+		<div class="article">
+			<h2>PHP</h2>
+			<p>PHP is a general-purpose scripting language widely used as a server-side language for creating dynamic web pages. Though its reputation is mixed, PHP is still extremely popular and is used in over 75% of all websites where the server-side programming language is known.</p>
+			<p>10 Best PHP Online Courses for Beginners to learn in 2021
+			PHP for Beginners [Udemy best Course] ...
+			Build a Social Network from Scratch [Udemy] ...
+			Web Applications for Everybody [Coursera] ...
+			PHP Fundamentals [Pluralsight] ...
+			PHP with Laravel for beginners [Udemy Course] ...
+			Object Oriented PHP & MVC. ...
+			PHP Unit Testing with PHPUnit.</p>
+
+			<h2>CSS</h2>
+			<p>Image result for about php course
+				In order to start PHP, MySQL, PHPMyAdmin and Apache in a single attempt, XAMPP should be installed. Scroll over to XAMPP for Windows and download should begin shortly. Click the .exe file to start the installation procedure. Select the components which you want to install and click “Next”.</p>
+		</div>
+
+		<!-- Footer section Starts -->
+	<div class="footer">
+		<div class="wrapper">
+			<p class="text-center">2020 All rights Developed By -<a href="https://www.facebook.com/tanvirhassan.nahid/">NAHID</a></p>
+		</div>
+		
+	</div>
+	<!-- Footer section Ends -->
+
+
+
+</body>
+</html>
